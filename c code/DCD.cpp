d8fdc160e18632155f8c7e60fd0315e3325a17fe
moulygupta23 <moulygupta23@gmail.com>
2016-05-10 06:12:43 +0530

    Dcd code ready without shrinking with random shuffling

--- c code/DCD.cpp
+++ c code/DCD.cpp
@@ -5,7 +5,9 @@
 #include <fstream>
 #include <string>
 #include <sstream>
-#define MAXITR 50;
+#include <ctime>
+
+#define MAXITR 400
 
 using namespace std;
 
@@ -60,6 +62,7 @@ public:
             if (count > n)
                 n=count;
         }
+        cout<<"reading file complete\n\n";
     }
 
     void printData(){
@@ -77,49 +80,188 @@ public:
 class TrainingModel{
 public:
     bool converge;
+    float lambda,c1,c2,w0;
     vf alpha,alphaold,beta,betaold,w,Ga,Gb,qii;
+    int nsv;
 
-    TrainingModel(){}
-
-    TrainingModel(Data tr){
+    vl initialzation(Data tr){
         alpha=vf(tr.m,1.0/tr.m);
         alphaold=alpha;
         beta=vf(tr.m,0);
         betaold=beta;
         w=vf(tr.n,0);
-
+        w0=0;
+        converge=false;
+        vl seq;
+        // qii is sum of sqare of each data point(sum of sqaure of each feature)
+        // qii=sum(x(i,:).^2,2);
+        // w=(y.*(beta-alpha))'*x;
         for (li i=0;i<tr.m;i++){
+            float Q=1.0f;
             for(li j=0;j<tr.ind[i].size();j++){
-                w[tr.ind[i][j]]+=(tr.y[i]*(beta[i]-alpha[i])*tr.x[i][(tr.ind[i][j])]);
+                w[tr.ind[i][j]]+=(tr.y[i]*(beta[i]-alpha[i])*tr.x[i][j]);
+                Q+=(tr.x[i][j]*tr.x[i][j]);
+            }
+            w0+=(tr.y[i]*(beta[i]-alpha[i]));
+            qii.push_back(Q);
+            seq.push_back(i);
+
+        }
+        //printModel(tr.m,tr.n);
+        // int u;
+        // cin>>u;
+        lambda=23.5f;
+        c1=1.0f;
+        c2=1.0f;
+        cout<<"initialzation done\n";
+        return seq;
+    }
+
+    void train(Data tr){
+    	vl seq=initialzation(tr);
+    	clock_t begin, end;
+		double time_spent;
+
+		begin = clock();
+        li changedvariable=0;
+        float dela,delb;
+        int itr=0;
+        while(!converge){
+            changedvariable=0;
+            random_shuffle(seq.begin(), seq.end());
+            for(li k=0;k<tr.m;k++){
+            	li i=seq[k];
+                float Gb=w0;
+                dela=0,delb=0;
+                for(li j=0;j<tr.ind[i].size();j++)
+                    Gb+=(tr.x[i][j]*w[tr.ind[i][j]]);
+                Gb=tr.y[i]*Gb-1;
+                float Ga=lambda-Gb-1;
+                //cout<<i+1 <<" Ga ="<<Ga<<" Gb="<<Gb<<" qii="<<qii[i]<<endl;
+                bool fa=true,fb=true;
+                // updating alpha
+                if(fabs(Ga)<1e-4)
+                    fa=false;
+                else if(alpha[i]<=1e-5 && Ga >= 0)
+                    fa=false;
+                else if(alpha[i]>=c2-1e-5 && Ga <= 0)
+                    fa=false;
+                else
+                    fa=true;
+                if(fa){
+                    alphaold[i]=alpha[i];
+                    alpha[i]=min(max((alpha[i]-Ga/qii[i]),0.0f),c2);
+                    //cout<<alpha[i]<<endl;
+                    dela=alpha[i]-alphaold[i];
+                    if(fabs(dela)>=1e-4)
+                        changedvariable++;
+                }
+
+                // updating beta
+                if(fabs(Gb)<1e-4)
+                    fb=false;
+                else if(beta[i]<=1e-5 && Gb >= 0)
+                    fb=false;
+                else if(beta[i]>=c1-1e-5 && Gb <= 0)
+                    fb=false;
+                else
+                    fb=true;
+                if(fb){
+                    betaold[i]=beta[i];
+                    beta[i]=min(max((beta[i]-Gb/qii[i]),0.0f),c1);
+                   	//cout<<beta[i]<<endl;
+                    delb=beta[i]-betaold[i];
+                    if(fabs(delb)>=1e-4)
+                        changedvariable++;
+                }
+
+                //updating w
+                if(fa||fb){
+                    for(li j=0;j<tr.ind[i].size();j++)
+                        w[tr.ind[i][j]]+=(tr.y[i]*(delb-dela)*tr.x[i][j]);
+                    w0+=(delb-dela)*tr.y[i];
+           //          for(li i=0;i<tr.n;i++){
+			        //     cout<<w[i]<<" ";
+			        // }
+			        // cout<<w0<<endl;
+                }
+
+            }
+            itr++;
+            if(itr==MAXITR || changedvariable==0){
+                cout<<"itr="<<itr<<"\t"<<"changedvariable="<<changedvariable<<"\n\n\n";
+                converge=true;
             }
         }
-
-//         w=(y.*(b1-a1))'*x;
-// j=0;
-// Gb=y.*(x*w')-1;
-// Ga=lambda-Gb-1;
-// Q=zeros(1,m);
-// %qii=<x(i,:),x(i,:)>
-// for i=1:m
-//     Q(i)=sum(x(i,:).^2);
-// end
-// disp('initialization complete');
+        nsv=0;
+        for(li i=0;i<tr.m;i++){
+        	if(beta[i]-alpha[i]!=0)
+        		nsv++;
+        }
+        end = clock();
+		time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+		cout<<"training complete\n\ntime for training : "<<time_spent<<"sec"<<endl;
     }
 
-
-    void initialization(){
-
-    }
     void printModel(li m,li n){
-        cout<<"alpha\tbeta\n";
+    	cout<<"nummber of support vector : "<<nsv<<endl;
+        //cout<<"alpha\tbeta\tqii\n";
+        float sumalpha=0;
         for (li i = 0; i < m; i++){
-            cout<<alpha[i]<<"\t"<<beta[i]<<endl;
+            //cout<<alpha[i]<<"\t"<<beta[i]<<"\t"<<qii[i]<<endl;
+            sumalpha+=alpha[i];
         }
-        cout<<"w : ";
+        cout<<"sum of alphas="<<sumalpha<<"\nw : ";
+        float margin=0;
         for(li i=0;i<n;i++){
-            cout<<w[i]<<" ";
+            //cout<<w[i]<<" ";
+            margin+=w[i]*w[i];
         }
-        cout<<endl;
+        //cout<<w0<<endl;
+        margin=1/sqrt(margin);
+        cout<<"margin = "<<margin<<endl;
+    }
+
+    void prediction(Data tt){
+    	long n1 = w.size();
+
+    	long n=min(n1,tt.n);
+
+    	//vector<int> pred;
+    	li correct=0;
+
+    	for(li i=0;i<tt.m;i++){
+    		float d=0;
+    		for(li j=0;j<n;j++){
+    			d+=tt.x[i][j]*w[tt.ind[i][j]];
+    		}
+    		if(n1==tt.n){
+    			d+=w0;
+    		}
+    		else if(n1<tt.n){
+    			//if the ind[n1] exist and is n1 then it is non-zero so mutiply x[ind[n1]] with w0
+    			if(tt.ind[i][n1]+1==n1)
+    				d+=w0*tt.x[i][n1];
+    		}
+    		else{
+    			//n1>nt
+    			d+=w[tt.n];
+    		}
+
+    		//if pred is 1 and the yt =1 then it is correct;
+    		if((d>=0&&tt.y[i]==1)||(d<0&&tt.y[i]==-1))
+    			correct++;
+    	}
+    	
+    	float accuracy=correct*100.0/tt.m;
+
+    	cout<<"accuracy="<<accuracy<<endl;
+
+
+    	/*
+margin=1/norm(w1)
+%figure
+%end*/
     }
 };
 
@@ -128,16 +270,21 @@ public:
 int main(){
     Data *tr=new Data();
     Data *tt=new Data();
+    //string trainfile="../../Data_ML/mnist38_norm_svm_full_1.train";//
     string trainfile="sparsedata1.train";//"kddb_unnorm_svm_1.train";
-    string testfile="sparsedata1.test";//"kddb_unnorm_svm_1.test";
+   // string testfile="../../Data_ML/mnist38_norm_svm_full_1.test";//
+   string testfile="sparsedata1.test";//"kddb_unnorm_svm_1.test";
 	tr->readinput(trainfile);
-    tr->printData();
-    TrainingModel *model=new TrainingModel(*tr);
+    //tr->printData();
+    // TrainingModel *model=new TrainingModel(*tr);
+    TrainingModel *model=new TrainingModel();
+    model->train(*tr);
     model->printModel(tr->m,tr->n);
     tt->readinput(testfile);
-    cout<<tr->m<<" "<<tr->n<<endl<<tt->m<<" "<<tt->n<<endl;
+    model->prediction(*tt);
+    //cout<<tr->m<<" "<<tr->n<<endl<<tt->m<<" "<<tt->n<<endl;
     //tt->printData();
-	cout<<"reading complete\n";
+	//cout<<"reading complete\n";
     delete tr;
     delete tt;
     delete model;
